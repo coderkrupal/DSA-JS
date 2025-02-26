8adfe18bc05e16e77ee12196d4be281e57ef0823
KRUPAL <krupaldoshi01@gmail.com>
2025-02-26 23:45:50 +0530

    basics

---
+++ MasterTheBasics/Mathfunc.js
@@ -0,0 +1,20 @@
+ // Math.round()
+//    Math.floor()
+//    Math.max()
+//    Math.ceil()
+//    Math.sqrt()
+//    Math.abs()
+//    Math.min()
+//    Math.random()
+
+let  a = 10;
+let  b = 20;
+
+console.log(Math.floor(1.80))
+console.log(Math.round(10.5))
+console.log(Math.sqrt(25))
+console.log(Math.min(a,b))
+console.log(Math.max(a,b))
+console.log(Math.pow(5))
+console.log(Math.trunc(19.90))
+console.log(Math.trunc(Math.round(Math.random() * 9000)+1000));
---
+++ MasterTheBasics/swapping.js
@@ -0,0 +1,24 @@
+// first method for swapping
+
+let  a = 30;
+let  b = 50;
+let c;
+
+c = a
+a = b
+b = c
+
+
+//second method for swapping
+
+  a = a + b;
+  b = a - b;
+  a = a -b;
+
+  //third method for swapping
+
+
+  [a,b] = [b,a]
+
+  console.log(a)
+  console.log(b)
